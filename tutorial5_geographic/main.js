198474e001850b5e2dc274d4a8c1b1792cc47a23
angelinatham <78704348+angelinatham@users.noreply.github.com>
2021-03-21 18:37:57 -0400

    commit

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -24,17 +24,19 @@ let state = {
  * LOAD DATA
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
-Promise.all([
-  d3.json("../data/usState.json"),
-  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
-]).then(([geojson, otherData]) => {
-  // + SET STATE WITH DATA
-  state.geojson = geojson
-  state.heat = otherData
-  console.log("state: ", state);
-  init();
-});
-    
+{
+  Promise.all([
+    d3.json("../data/usState.json"),
+    d3.csv("../data/usHeatExtremes.csv", d3.autoType),
+  ]).then(([geojson, otherData]) => {
+    // + SET STATE WITH DATA
+    state.geojson = geojson
+    state.heat = otherData
+    console.log("state: ", state);
+    init();
+  });
+}
+
 
 /**
  * INITIALIZING FUNCTION
@@ -42,14 +44,15 @@ Promise.all([
  * */
 function init() {
 
-    // + SET UP PROJECTION
-    const projection = d3.geoAlbersUsa()
-    .fitSize([width,height], state.geojson)
+  // + SET UP PROJECTION
+  const projection = d3.geoAlbersUsa()
+    .fitSize([width, height], state.geojson)
 
+  radius = d3.scaleLinear([0, d3.max(state.heat, d => d.TempChange)], [3, 13])
 
   //color?
-  //const colorScale = d3.scaleSequential(d3.interpolateBlues)
-//.domain(d3.extent(state.geojson.features, d => d.properties.AWATER))
+  const colorScale = d3.scaleSequential(d3.interpolateBlues)
+  //.domain(d3.extent(state.geojson.features, d => d.properties.AWATER))
   // create an svg element in our main `d3-container` element
   svg = d3
     .select("#d3-container")
@@ -57,25 +60,29 @@ function init() {
     .attr("width", width)
     .attr("height", height);
 
-    const states = svg.selectAll("path")
-      .data(state.geojson.features)
-      .join("path")
-      .attr("stroke-width", "2")
-      .attr("fill" , "#CCABD8")
-      .attr("d", pathFunction)
+  const pathFunction = d3.geoPath(projection)
+  const states = svg.selectAll("path")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("stroke-width", "2")
+    .attr("fill", "#CCABD8")
+    .attr("d", pathFunction)
 
   // + SET UP GEOPATH
-const pathFunction = d3.geoPath(projection)
-svg.selectAll("path")
-  .data(state.geojson.features)
-  .join("path")
-  .attr("stroke", "black")
-  .attr("d", pathFunction)
-  .attr("fill", d => {
-    return colorScale(d.properties.AWATER)
-  })
+
+  svg.selectAll("path")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("stroke", "black")
+    .attr("d", pathFunction)
+    .attr("fill", d => {
+      return colorScale(d.properties.AWATER)
+    })
 
   // + DRAW BASE MAP PATH
+
+
+
   // + ADD EVENT LISTENERS (if you want)
 
   draw(); // calls the draw function
@@ -85,24 +92,24 @@ svg.selectAll("path")
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
-function draw() {}
 
-//circle
-svg.selectAll("circle.point")
-.data(state.heat)
-.join("circle")
-.attr("fill", d => {
-  if (d.TempChange > 0) return "#8474A1";
-  else if (d.TempChange === 0 ) return "#055B5C"
-  else return "#6EC6CA"
-.attr("r", d => radius(Math.abs(d.TempChange)))
-.attr("r", 3)
-.attr("stroke", "#ccc")
-
-})
-
+function draw() {
+  svg.selectAll("circle")
+    .data(state.heat)
+    .join("circle")
+    .attr("fill", d => {
+      if (d.TempChange > 0) return "#8474A1";
+      else if (d.TempChange === 0) return "#055B5C"
+      else return "#6EC6CA"
+    })
+    .attr("r", d => radius(Math.abs(d.TempChange)))
+    .attr("r", 3)
+    .attr("stroke", "#ccc")
+}
 
 
-  
-  radius = d3.scaleLinear([0, d3.max(state.heat, d => d.TempChange)], [3, 13])
-})
+
+
+
+
+
